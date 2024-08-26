a3ca6ded7d647302fc1ef08f64b95c49025d3cf9
kenblu24 <kenblu24@hotmail.com>
2024-08-26 13:17:25 -0400

    add old cma best

--- controllers/choice.py
+++ controllers/choice.py
@@ -3,11 +3,15 @@ from . import BaseController
 from . import pid
 
 
+genome = [0.08582296707938367, 0.12810866517604869, 0.19527305555877467, -0.10766175831842266]
+
+
 class Controller(BaseController):
     def __init__(self):
         # self.controller = pid.PIDController(p=0.12, i=0.115, d=0.005, scale=1, imax=1)
         # self.controller = pid.PID4Controller(p=0.12, i=0.115, d=0.005, d2=-0.0008, scale=1, imax=1)
-        self.controller = pid.PIDDController()
+        self.controller = pid.PID4Controller(*genome, scale=1, imax=1)
+        # self.controller = pid.PIDDController()
 
     @override
     def update(self, *args, **kwargs):
